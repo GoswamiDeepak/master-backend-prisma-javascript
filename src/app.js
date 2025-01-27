1da24cf4a08f15e88cff38135f91cdfc4afd3c9e
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2025-01-27 16:47:57 +0530

    add sample homepage endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 
-const app = express()
+const app = express();
 
+app.get('/', (req, res) => {
+    return res.json({ message: 'Welcome to Master Backend' });
+});
 
 export default app;
