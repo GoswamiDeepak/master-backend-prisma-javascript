09b01ed8098c66ae90de58f5bba5cb72709894a3
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2025-01-29 01:20:22 +0530

    add auth middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+import createHttpError from 'http-errors';
+import jwt from 'jsonwebtoken';
+import { config } from '../config/index.js';
+
+const auth = async (req, _, next) => {
+    const accessToken =
+        req.cookies.accessToken ||
+        req.headers.authorization?.replace('Bearer ', '');
+    if (!accessToken) {
+        return next(createHttpError(403, 'Invalid access!'));
+    }
+    const isUser = jwt.verify(accessToken, config.secretKey);
+    if (!isUser) {
+        return next(createHttpError(401, 'Token expired!.'));
+    }
+    const user = {
+        id: isUser.id,
+        email: isUser.email,
+        name: isUser.name,
+        profile: isUser.profile,
+    };
+    req.user = user;
+    next();
+};
+
+export default auth;
